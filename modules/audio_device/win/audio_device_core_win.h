4b534385cbd456ad8604571655fcc67534b99f29
taste1981 <jianlin.qiu@intel.com>
2020-07-22 17:12:26 +0800

    Enable automatic audio playback device switching (#92)

--- modules/audio_device/win/audio_device_core_win.h
+++ modules/audio_device/win/audio_device_core_win.h
@@ -76,12 +76,18 @@ class ScopedCOMInitializer {
   void operator=(const ScopedCOMInitializer&);
 };
 
-class AudioDeviceWindowsCore : public AudioDeviceGeneric {
+class AudioDeviceWindowsCore : public AudioDeviceGeneric,
+                               public IMMNotificationClient {
  public:
   AudioDeviceWindowsCore();
   ~AudioDeviceWindowsCore();
 
   static bool CoreAudioIsSupported();
+
+  // IUnknown (required by IMMNotificationClient).
+  ULONG __stdcall AddRef() override;
+  ULONG __stdcall Release() override;
+  HRESULT __stdcall QueryInterface(REFIID iid, void** object) override;
 
   // Retrieve the currently utilized audio layer
   virtual int32_t ActiveAudioLayer(
@@ -262,6 +268,7 @@ class AudioDeviceWindowsCore : public AudioDeviceGeneric {
   HANDLE _hPlayThread;
   HANDLE _hRenderStartedEvent;
   HANDLE _hShutdownRenderEvent;
+  HANDLE _hDeviceRestartEvent;
 
   HANDLE _hCaptureSamplesReadyEvent;
   HANDLE _hRecThread;
@@ -292,6 +299,30 @@ class AudioDeviceWindowsCore : public AudioDeviceGeneric {
   double _perfCounterFactor;
 
  private:
+  // IMMNotificationClient implementation. At present we
+  // only handle OnDefaultDeviceChanged event.
+  HRESULT __stdcall OnDeviceStateChanged(LPCWSTR pwstrDeviceId,
+                                         DWORD dwNewState) override {
+    return S_OK;
+  }
+
+  HRESULT __stdcall OnDeviceAdded(LPCWSTR pwstrDeviceId) override {
+    return S_OK;
+  }
+
+  HRESULT __stdcall OnDeviceRemoved(LPCWSTR pwstrDeviceId) override {
+    return S_OK;
+  }
+
+  HRESULT __stdcall OnDefaultDeviceChanged(
+      EDataFlow flow,
+      ERole role,
+      LPCWSTR pwstrDefaultDeviceId) override;
+
+  HRESULT __stdcall OnPropertyValueChanged(LPCWSTR pwstrDeviceId,
+                                           const PROPERTYKEY key) override {
+    return S_OK;
+  }
   bool _initialized;
   bool _recording;
   bool _playing;
@@ -306,6 +337,7 @@ class AudioDeviceWindowsCore : public AudioDeviceGeneric {
   AudioDeviceModule::WindowsDeviceType _outputDevice;
   uint16_t _inputDeviceIndex;
   uint16_t _outputDeviceIndex;
+  LONG ref_count_ = 1;
 };
 
 #endif  // #if (_MSC_VER >= 1400)
