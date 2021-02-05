open-webrtc-toolkit/owt-deps-webrtc

commit 51c8bd94cca88a248bd6ad9deeddb9b625998ecb

Bugfix and minor code improvement (#122)

video/rtp_video_stream_receiver.cc
@@ -628,9 +628,8 @@ void RtpVideoStreamReceiver::OnReceivedPayloadData(
         packet->video_payload = std::move(fixed.bitstream);
         break;
     }
-  }
 #ifndef DISABLE_H265
-  else if (packet->codec() == kVideoCodecH265) {
+  } else if (packet->codec() == kVideoCodecH265) {
     // Only when we start to receive packets will we know what payload type
     // that will be used. When we know the payload type insert the correct
     // sps/pps into the tracker.
@@ -657,7 +656,7 @@ void RtpVideoStreamReceiver::OnReceivedPayloadData(
     }
   }
 #endif
-  else {
+  } else {
     packet->video_payload = std::move(codec_payload);
   }
 

modules/video_coding/session_info.cc
@@ -519,9 +519,8 @@ int VCMSessionInfo::InsertPacket(const VCMPacket& packet,
          IsNewerSequenceNumber(packet.seqNum, last_packet_seq_num_))) {
       last_packet_seq_num_ = packet.seqNum;
     }
-#else
-  } else {
 #endif
+  } else {
     // Only insert media packets between first and last packets (when
     // available).
     // Placing check here, as to properly account for duplicate packets.

sdk/objc/native/src/objc_video_encoder_factory.mm
@@ -64,7 +64,6 @@ class ObjCVideoEncoder : public VideoEncoder {
             [(RTC_OBJC_TYPE(RTCCodecSpecificInfoH264) *)info nativeCodecSpecificInfo];
 #ifndef DISABLE_H265
       } else if ([info isKindOfClass:[RTC_OBJC_TYPE(RTCCodecSpecificInfoH265) class]]) {
-        // if ([info isKindOfClass:[RTCCodecSpecificInfoH265 class]]) {
         codecSpecificInfo = [(RTCCodecSpecificInfoH265 *)info nativeCodecSpecificInfo];
 #endif
       }

commit 2619b2535ce802ac36466a5f7141e40903691ba1

Fix ci failure caused by pr #122 (#124)
* Fix build failure on VS17

* Revert build failure caused by PR #122

video/rtp_video_stream_receiver.cc
@@ -654,7 +654,6 @@ void RtpVideoStreamReceiver::OnReceivedPayloadData(
         packet->video_payload = std::move(fixed.bitstream);
         break;
     }
-  }
 #endif
   } else {
     packet->video_payload = std::move(codec_payload);