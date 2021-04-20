073540b46e23da7898b69e0e5f7fbfbf332e7457
Johny Qiu <jianlin.qiu@intel.com>
2021-04-20 14:21:08 +0800

    Fix android build errors after DiffServ changes. (#139)

--- api/media_types.cc
+++ api/media_types.cc
@@ -26,6 +26,8 @@ std::string MediaTypeToString(MediaType type) {
       return kMediaTypeVideo;
     case MEDIA_TYPE_DATA:
       return kMediaTypeData;
+    case MEDIA_TYPE_SCREEN:
+      return kMediaTypeScreen;
     case MEDIA_TYPE_UNSUPPORTED:
       // Unsupported media stores the m=<mediatype> differently.
       RTC_DCHECK_NOTREACHED();
